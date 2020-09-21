78d184b16dfba907a98577ee6ce60d23cb74b5ca
Alex Kamenev <kamesash251@gmail.com>
2020-09-21 23:35:46 +1100

    the solution of the day 1

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
+        for(int a = 0;a != 10;a++){
+            System.out.print("JAVA ");
+        }
 
     }
 }
---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task6 {
+    public static void main(String args[]){
+        int k = 7;
+        int a = 1;
+        while(a <= 9){
+            System.out.println(a + " * " + k + " = " + a*k);
+            a = a + 1;
+        }
+    }
+}
